db6b14101d2fd5e72c539e68d7a5e5f84faffd69
l0k1 <justi.n.icholson@gmail.com>
2015-09-11 00:44:03 -0700

    Moving source into a src/ file.

--- fonts/fonts.asm
+++
@@ -1,9 +0,0 @@
-;* This file will contain all the tile data for fonts that will be used.
-;* Font data will go in ROM bank 1 (for now).
-
-   SECTION "Font_Main",ROM1
-   ;Font_Main will have A-Z,a-z,0-9, and most punctuation, for now.
-   ;If a character doesn't get used, it'll be cut from the game.
-Font_Main::
-
-End_Font_Main::
