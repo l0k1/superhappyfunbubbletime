f166c333398fde2363234244496dc4933c15e3f1
l0k1 <justi.n.icholson@gmail.com>
2015-09-12 10:52:24 -0700

    More progress on opening screens. Not yet complete.

--- src/opening_screens.asm
+++ src/opening_screens.asm
@@ -22,14 +22,60 @@ Splash_Screen::
    jr nz,.load_font_loop
 
 ;fill up the first 5 rows with a solid block
-   ld BC,_SCRN0      ; Loading the map into screen 0
+   ld HL,_SCRN0      ; Loading the map into screen 0
+   ld BC,Title_Screen_Map
    ld DE,32*5        ; 32 * 5 = first 5 rows
-.first_five_loop
-   ld A,$5F          ; $5F is the solid block char.
-   ld [BC],A
-   dec DE
-   ld A,D
-   or E
-   jr nz,.first_five_loop
+   ld B,$5F
+   call Load_Blanks
+
+;line 6, and the first 7 spaces of line 7, are blank.
+   ld DE,32+7         ; One entire line + 7 on the next line
+   ld B,0
+   call Load_Blanks
+
+;now load in "Klexos"
+   ld BC,Title_Screen_Map
+   ld E,6
+.klexos_loop
+   ld A,[BC]
+   ld [HL+],A
+   dec E
+   xor A
+   cp E
+   jr nz,.klexos_loop
+
+;now do 19+8 more blank spaces
+   ld B,0
+   ld DE,19+8
+   call Load_Blanks
+
+;now do "Game"
+   ld BC,Title_Screen_Map + $06
+   ld E,4
+.game_loop
+   ld A,[BC]
+   ld [HL+],A
+   dec E
+   xor A
+   cp E
+   jr nz,.game_loop
+
+;now do 20 + 7 more blank spaces
+   ld B,0
+   ld DE,20+7
+   call Load_Blanks
+
+;A function for filling in with a certain tile
+;Count needs to be in DE
+;The tile needs to be in B
+;HL needs to point at the BG Map area.
+Load_Blanks:
+.loop
+   ld A,B
+   ld [HL+],A
+   ld A,E
+   or D
+   jr nz,.loop
+   ret
 
 End_Splash_Screen::
