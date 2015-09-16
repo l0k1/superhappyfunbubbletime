f82a6dfc65393bc4d91f05d950823623842e796a
Justin Nicholson <justi.n.icholson@gmail.com>
2015-09-15 23:29:09 -0700

    Fixing the lettering in the splash screen.

--- src/maps/opening_maps.asm
+++ src/maps/opening_maps.asm
@@ -8,14 +8,14 @@ Title_Screen_Map::
 ;the first 5 lines $5F
 ;line 6 is all $00
 ;line 7: "Klexos" (7 spaces before, 19 after)
-DB $2B,$4C,$45,$58,$4F,$53
+DB $2C,$4C,$45,$58,$4F,$53
 ;line 8: "Game" (8 spaces before, 20 after)
-DB $27,$41,$4D,$45
+DB $28,$41,$4D,$45
 ;line 9: "Studio" (7 spaces before, 19 after)
-DB $33,$54,$55,$44,$49,$4F
+DB $34,$54,$55,$44,$49,$4F
 ;lines 10 - 11 are blank
 ;line 12: "Presents" (6 spaces before, 18 after)
-DB $30,$52,$45,$53,$45,$4E,$54,$53
+DB $31,$52,$45,$53,$45,$4E,$54,$53
 ;line 13 is blank
 ;lines 14-18 are $5F
 End_Title_Screen_Map::
