778c848c2f1467a24e18ae037904ea65fa22e116
Justin Nicholson <justi.n.icholson@gmail.com>
2016-01-07 23:42:41 +0000

    some tiles.

---
+++ src/tiles/landscape.asm
@@ -0,0 +1,48 @@
+;landscape tiles - all the outdoor tiles!
+;programmer art mostly. don't hate me!
+;these have been done at work with paint/excel. 
+;i need to put these into .gbr format, and upload for later use.
+
+ SECTION "Normal Tileset - Grass Tiles", HOME
+
+;grass1, standard grass tile
+grass1:
+DB $00,$00,$00,$00,$50,$00,$50,$00
+DB $00,$00,$0A,$00,$0A,$00,$00,$00
+
+;grass2, same as grass1 but upside down
+grass2:
+DB $00,$00,$0A,$00,$0A,$00,$00,$00
+DB $00,$00,$00,$00,$50,$00,$50,$00
+
+;flower1, some subdued flowers
+flower1:
+DB $00,$00,$20,$00,$50,$00,$24,$00
+DB $0A,$00,$44,$00,$A0,$00,$40,$00
+
+;flower2, mirrored flower1
+flower2:
+DB $00,$00,$04,$00,$0A,$00,$24,$00
+DB $50,$00,$22,$00,$05,$00,$02,$00
+
+;boulder1, just a big ol rock.
+boulder1:
+DB $3E,$3E,$C1,$C1,$89,$81,$85,$81
+DB $A1,$81,$91,$81,$81,$81,$42,$7E
+
+;tree1, a smallish deciduous tree. prime example of programmer art.
+tree1_upperleft:
+DB $0F,$0F,$30,$30,$42,$40,$91,$80
+DB $8A,$80,$51,$40,$88,$80,$80,$80
+
+tree1_lowerleft:
+DB $60,$60,$1F,$1F,$04,$04,$04,$05
+DB $04,$05,$04,$04,$04,$04,$08,$08
+
+tree1_upperright:
+DB $68,$68,$94,$94,$02,$02,$22,$02
+DB $91,$01,$09,$01,$91,$01,$09,$01
+
+tree1_lowerright:
+DB $82,$82,$7C,$7C,$20,$20,$20,$20
+DB $20,$60,$20,$60,$30,$30,$08,$08
